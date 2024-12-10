a91718e31e620fcec5b4115dbc08ddc6a73602e1
evgeny <afazhenya2001@gmail.com>
2024-12-10 20:44:46 +0300

    v-bind in style sfc

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <script setup>
 import { ref } from 'vue';
+const colors = ["blue", "yellow", "red", "green"]
 
-const counter = ref(0);
+const theme = ref("red")
 
 setInterval(() => {
-  counter.value++
+  theme.value = colors[Math.floor(Math.random() * 4)]
 }, 1000)
 
 </script>
 
 <template>
-  <button @click="counter++" v-once>
-    Click: {{ counter }}
-  </button>
+  <p :style="{color: theme}">{{ theme }}</p>
 </template>
 
 <style scoped>
-
+p {
+  color: v-bind(theme)
+}
 </style>
