bdb0015505682c5d12bf83f1a4921840d1ad2384
evgeny <afazhenya2001@gmail.com>
2024-12-10 19:03:58 +0300

    v-once

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script setup>
+import { ref } from 'vue';
+
+const counter = ref(0);
+
+setInterval(() => {
+  counter.value++
+}, 1000)
+
 </script>
 
 <template>
+  <button @click="counter++" v-once>
+    Click: {{ counter }}
+  </button>
 </template>
 
 <style scoped>
