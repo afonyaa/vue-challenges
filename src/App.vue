13f6b74a97bda773586cb701d1f6660532b0277b
evgeny <afazhenya2001@gmail.com>
2024-12-10 22:41:57 +0300

    prop validation

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,13 @@
 <script setup>
+import MyButton from './components/MyButton.vue';
 
 
 </script>
 
 <template>
-   <p>Hello Vue.js</p>
+  <MyButton type="success"/>
 </template>
 
 <style scoped>
-p {
-  font-size:20px;
-  color:red;
-  text-align: center;
-  line-height: 50px;
-}
-
-/* Make it work */
-:global(body) {
-  width: 100vw;
-  height: 100vh;
-  background-color: burlywood;
-}
 
 </style>
---
+++ src/components/MyButton.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  type: {
+    validator(value, props) {
+      // Значение должно соответствовать одной из этих строк
+      return ['success', 'warning', 'danger'].includes(value)
+    }
+  }
+})
+</script>
+
+<template>
+    <button>
+        button
+    </button>
+</template>
+
+<style scoped>
+
+</style>
