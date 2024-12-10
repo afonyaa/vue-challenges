756bb990f57b975152ad1f3e3d7b77866d111d98
evgeny <afazhenya2001@gmail.com>
2024-12-11 00:58:26 +0300

    pure functions

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,54 @@
 <script setup>
-import MyButton from './components/MyButton.vue';
+import { ref } from 'vue';
+import { h } from 'vue'; 
 
+
+const ListComponent = (props, context) => {
+  return h('ul', 
+      props.list.map((el, idx) => 
+        h('div', 
+          [
+            h('input', {
+              type: "checkbox",
+              checked: props.activeIndex === idx, 
+              onChange: () => context.emit('toggle', idx)
+            }), 
+            h('span', el.name)
+          ]
+        )
+      )
+  )
+}
+
+ListComponent.props = {
+  list: {
+    type: Array
+  },
+  activeIndex: {
+    type: Number
+  }
+}
+
+const list = [
+  { name: "John" }, 
+  { name: "Doe" }, 
+  { name: "Smith" }
+]
+
+const activeIndex = ref(0)
+
+const toggle = (idx) => {
+  activeIndex.value = idx
+}
 
 </script>
 
 <template>
-  <MyButton type="success"/>
+   <ListComponent
+    :list="list"
+    :active-index="activeIndex"
+    @toggle="toggle"
+  />
 </template>
 
 <style scoped>
