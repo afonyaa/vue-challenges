c730f74800fce6cb2dc11613058dca3a1b65fb94
evgeny <afazhenya2001@gmail.com>
2024-12-11 19:56:43 +0300

    shallow ref

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,22 @@
 <script setup lang="ts">
-import { ref, watch } from "vue"
+import { shallowRef, watch } from "vue"
 
-const count = ref(0)
+const state = shallowRef({ count: 1 })
 
-/**
- * Challenge 1: Watch once
- * Make sure the watch callback is only triggered once
-*/
-watch(count, () => {
-  console.log("Only triggered once")
-}, {
-  once: true
-})
-
-count.value = 1
-setTimeout(() => count.value = 2)
-
-/**
- * Challenges 2: Watch object
- * Make sure the watch callback is triggered
-*/
-const state = ref({
-  count: 0,
-})
-
+// Does NOT trigger
 watch(state, () => {
-  console.log("The state.count updated")
-}, {
-  deep: true
-})
-
-state.value.count = 2
+  console.log("State.count Updated")
+}, { deep: true })
 
 /**
- * Challenge 3: Callback Flush Timing
- * Make sure visited the updated eleRef
+ * Modify the code so that we can make the watch callback trigger.
 */
-
-const eleRef = ref()
-const age = ref(2)
-
-watch(age, () => {
-  console.log(eleRef.value)
-}, {
-  flush: 'post'
-})
-
-age.value = 18
+state.value = {count: 2}
 
 </script>
 
 <template>
   <div>
-    <p>
-      {{ count }}
-    </p>
-    <p ref="eleRef">
-      {{ age }}
-    </p>
+    {{ state.count }}
   </div>
 </template>
