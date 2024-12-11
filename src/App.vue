7e6adc973015f97a54f28e054fde67f6520cd381
evgeny <afazhenya2001@gmail.com>
2024-12-11 12:45:52 +0300

    tree

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,39 @@
 <script setup>
+import { MyTree } from './components/MyTree';
 
 </script>
 
 <template>
+    <MyTree
+     :data="{
+        key: 0,
+        title: 'Node 0',
+        children: [
+            {
+                key: 1,
+                title: 'Node 1',
+                children: [
+                    {
+                        key: 3,
+                        title: 'Node 3',
+                        children: [
+                            {
+                                key: 4,
+                                title: 'Node 4',
+                                children: []
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                key: 2,
+                title: 'Node 2',
+                children: []
+            },
+        ]  
+      }"
+    />
 </template>
 
 <style scoped>
--- src/components/MyTree.js
+++ src/components/MyTree.js
@@ -1,5 +1,21 @@
 import {h} from 'vue'
 
 export const MyTree = (props, context) => {
-    return h('div')
+    return h('div', renderChild(props.data))
+}
+
+let renderChild = (node) => {
+    console.log(node)
+    if (node.children.length === 0) {
+        return h('ul', node.title)
+    }
+    return h('ul', [
+        node.title, node.children.map(childNode => h('li', renderChild(childNode)))
+    ])
+}
+
+MyTree.props = {
+    data: {
+        type: Object
+    }
 }
