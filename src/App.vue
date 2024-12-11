8e638416958f18f512d118ba3b41ed33e82f2575
evgeny <afazhenya2001@gmail.com>
2024-12-11 14:19:07 +0300

    toggle

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,31 @@
 <script setup>
-import { MyTree } from './components/MyTree';
+import { ref, watchEffect, onUnmounted, onMounted } from 'vue';
+
+
+const useToggle = (initialValue) => {
+    const state = ref(initialValue)
+    const toggle = () => {
+        console.log('toggle', state.value)
+        state.value = !state.value
+    }
+    return {state, toggle}
+}
+
+const {state, toggle} = useToggle(false)
+
+watchEffect(() => {
+    console.log(state)
+})
 
 </script>
 
 <template>
-    <MyTree
-     :data="{
-        key: 0,
-        title: 'Node 0',
-        children: [
-            {
-                key: 1,
-                title: 'Node 1',
-                children: [
-                    {
-                        key: 3,
-                        title: 'Node 3',
-                        children: [
-                            {
-                                key: 4,
-                                title: 'Node 4',
-                                children: []
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                key: 2,
-                title: 'Node 2',
-                children: []
-            },
-        ]  
-      }"
-    />
+    <p>
+        State: {{ state ? 'ON': 'OFF' }}
+    </p>
+    <button @click="toggle">
+        Toggle state
+    </button>
 </template>
 
 <style scoped>
