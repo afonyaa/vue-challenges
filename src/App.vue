9337610894d8423998e9a4148e45bf6422ee45f0
evgeny <afazhenya2001@gmail.com>
2024-12-11 18:25:18 +0300

    refs

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,64 @@
-<script setup>
-import { ref, provide } from "vue"
-import ChildComponent from "./components/ChildComponent.vue";
+<script setup lang="ts">
+import { ref, Ref, reactive, isRef, toValue, toRef } from "vue"
 
-const timer = ref(0)
+const initial = ref(10)
 const count = ref(0)
 
-provide("timer", timer)
-provide("count", count)
+// Challenge 1: Update ref
+function update(value) {
+    count.value = value
+}
+
+/**
+ * Challenge 2: Checks if `count` is a ref object.
+ * Make the output to be 1
+*/
+console.log(
+  isRef(count) ? 1 : 0
+)
+
+/**
+ * Challenge 3: Unwrap ref
+ * Make the output to be true
+*/
+function initialCount(value: number | Ref<number>) {
+  // Make the output to be true
+  console.log(toValue(value) === 10)
+}
+
+initialCount(initial)
+
+/**
+ * Challenge 4:
+ * create a ref for a property on a source reactive object.
+ * The created ref is synced with its source property:
+ * mutating the source property will update the ref, and vice-versa.
+ * Make the output to be true
+*/
+const state = reactive({
+  foo: 1,
+  bar: 2,
+})
+const fooRef = ref(toRef(state, 'foo')) // change the impl...
+
+
+// mutating the ref updates the original
+fooRef.value++
+console.log(state.foo === 2)
+
+// mutating the original also updates the ref
+state.foo++
+console.log(fooRef.value === 3)
 
 </script>
 
 <template>
-    <ChildComponent/>
+  <div>
+    <h1>msg</h1>
+    <p>
+      <span @click="update(count-1)">-</span>
+      {{ count }}
+      <span @click="update(count+1)">+</span>
+    </p>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
