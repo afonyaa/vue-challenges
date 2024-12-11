9c9701d002933d8c9187b7538ac811dd165caf0a
evgeny <afazhenya2001@gmail.com>
2024-12-11 23:11:35 +0300

    v-model-modifiers

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,11 @@
 <script setup>
-import { watch, customRef } from "vue"
+import { defineModel, ref} from "vue"
+import MyInput from "./components/MyInput.vue";
 
-function useDebouncedRef(factory, delay) {
-  let localValue = factory
-  let timeout
-  return customRef((track, trigger) => ({
-    get() {
-      track()
-      return localValue
-    },
-    set(value) {
-      localValue = value
-      clearTimeout(timeout)
-      timeout = setTimeout(() => {
-        trigger()
-      }, delay)
-    }
-  }))
-}
-const text = useDebouncedRef("hello", 2000)
-
-watch(text, (value) => {
-  console.log(value)
-})
+const val = ref('')
 
 </script>
 
 <template>
-    <input v-model="text" />
+  <MyInput v-model.capitalize="val" />
 </template>
