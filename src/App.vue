05e5d3b0f2d3664e32963ed565971521592acdc2
evgeny <afazhenya2001@gmail.com>
2024-12-18 15:19:23 +0300

    v-model

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,22 @@
 <script setup>
 import { ref, watch, watchEffect } from "vue"
 
-/**
- * Implement the custom directive
- * Make sure the list item text color changes to red when the `toggleTab` is toggled
- *
-*/
-
-const VActiveStyle = {
+const VOhModel = {
   mounted(el, binding) {
-    const stylesOriginal = el.style
-    console.log(stylesOriginal)
-    watchEffect(()=> {
-      console.log(binding.value)
-      const [styles, condition] = binding.value
-      if (condition()) {
-        for (const styleProp of Object.keys(styles)) {
-          el.style[styleProp] = styles[styleProp]
-        }
-      }
-      else {
-        for (const styleProp of Object.keys(styles)) {
-          console.log(stylesOriginal[styleProp])
-          el.style[styleProp] = ''
-        }
-      }
+    el.value = binding.value
+    el.addEventListener('input', (e) => {
+      console.log('listen', e.target.value)
+      value.value = e.target.value
     })
   }
 }
 
-const list = [1, 2, 3, 4, 5, 6, 7, 8]
+const value = ref("Hello Vue.js")
 
-const activeTab = ref(0)
-
-function toggleTab(index) {
-  activeTab.value = index
-}
-//todo реализовать без функции
 
 </script>
 
 <template>
- <ul>
-  <li
-      v-for="(item,index) in list"
-      :key="index"
-      v-active-style="[{'color':'red'}, () => activeTab === index]"
-      @click="toggleTab(index)"
-    >
-      {{ item }}
-    </li>
- </ul>
+  <input v-oh-model="value" type="text" />
+  {{ value }}
 </template>
