453f6cd11760cf1e0e8d99586493f16878e6d1d3
evgeny <afazhenya2001@gmail.com>
2024-12-18 15:29:11 +0300

    key-modifiers

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,16 @@
 <script setup>
 import { ref, watch, watchEffect } from "vue"
 
-const VOhModel = {
-  mounted(el, binding) {
-    el.value = binding.value
-    el.addEventListener('input', (e) => {
-      console.log('listen', e.target.value)
-      value.value = e.target.value
-    })
-  }
-}
-
-const value = ref("Hello Vue.js")
-
 
 </script>
 
 <template>
-  <input v-oh-model="value" type="text" />
-  {{ value }}
+    <!-- Add key modifiers made this will fire even if Alt or Shift is also pressed -->
+<button @click.alt.shift="onClick1">A</button>
+
+<!-- Add key modifiers made this will only fire when Shift and no other keys are pressed -->
+<button @click.shift.exact="onCtrlClick">A</button>
+
+<!-- Add key modifiers made this will only fire when no system modifiers are pressed -->
+<button @click.exact="onClick2">A</button>
 </template>
